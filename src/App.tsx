b3ade2ce6960ecb10b1118cbcd0ea761a51dfac3
guiluizmaia <68974994+guiluizmaia@users.noreply.github.com>
2020-11-30 21:24:28 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 
+import './App.css';
+
 function App() {
   return (
     <div className="bf">
       <div className="inputs">
-        <input placeholder={`teste`}/>
-        <input placeholder={`teste`}/>
-        <input placeholder={`teste`}/>
-        <input placeholder={`teste`}/>
+        <h1>Calculadora BF (Body Fat)</h1>
+        <h1>Peso:</h1>
+        <input placeholder={`Em KG`}/>
+        <h1>Altura:</h1>
+        <input placeholder={`Em Centímetros`}/>
+        <h1>Idade:</h1>
+        <input placeholder={`Em Anos`}/>
+        <h1>Pescoço:</h1>
+        <input placeholder={`Em Centímetros`}/>
+        <h1>Cintura:</h1>
+        <input placeholder={`Em Centímetros`}/>
+        <h1>Quadril (Para ulheres):</h1>
+        <input placeholder={`Em Centímetros`}/>
         <button >Calcular</button>
       </div>
     </div>
