03ccb4d8bf16f8b98935c2e306770b608f4e8d1e
guiluizmaia <68974994+guiluizmaia@users.noreply.github.com>
2020-12-02 20:42:02 -0300

    Add files via upload
    Adicionei as variaveis ao ambiente com useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 
 import './App.css';
 
 function App() {
+  const [peso, setPeso] = useState();
+  const [alt, setAlt] = useState();
+  const [ida, setIda] = useState();
+  const [pesc, setPesc] = useState();
+  const [cint, setCint] = useState();
+  const [quad, setQuad] = useState();
   return (
     <div className="bf">
       <div className="inputs">
         <h1 className="nome">Calculadora BF (Body Fat)</h1>
         <h1>Peso:</h1>
-        <input placeholder={`Em KG`}/>
+        <input value={peso} placeholder={`Em KG`}/>
         <h1>Altura:</h1>
-        <input placeholder={`Em Centímetros`}/>
+        <input value={alt} placeholder={`Em Centímetros`}/>
         <h1>Idade:</h1>
-        <input placeholder={`Em Anos`}/>
+        <input value={ida} placeholder={`Em Anos`}/>
         <h1>Pescoço:</h1>
-        <input placeholder={`Em Centímetros`}/>
+        <input value={pesc} placeholder={`Em Centímetros`}/>
         <h1>Cintura:</h1>
-        <input placeholder={`Em Centímetros`}/>
+        <input value={cint} placeholder={`Em Centímetros`}/>
         <h1>Quadril (Para ulheres):</h1>
-        <input placeholder={`Em Centímetros`}/>
+        <input value={quad} placeholder={`Em Centímetros`}/>
         <button>
           Calcular Para Mulheres
         </button>
